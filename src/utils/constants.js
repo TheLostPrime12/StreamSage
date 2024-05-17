fe3a5abddfb5910c71d8a39c425cb49888664054
TheLostPrime12 <tanishgarg1939@gmail.com>
2024-05-17 23:22:15 +0530

    Removed constants.js from tracking and added multiple lists according to genre

--- src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-export const API_OPTIONS = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMDJhY2VhZDBiYWFkNzJlZDk4MzZlNTI5YmNlZTVmZCIsInN1YiI6IjY2NDcxODA2Y2ZiNjI3NWFkMTRjNDRjYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WgDotlhjVvR9UYo0nJtczCRrve-JWGWmXTMnBY6T128",
-  },
-};
